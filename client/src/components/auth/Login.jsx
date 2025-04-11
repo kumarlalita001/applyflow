e9b32a033bdcb6683a0b27bfe3b95308b3e0c610
Lalit <kumarlalitamohanty@gmail.com>
2025-04-11 09:07:25 +0530

    useState import issue fixed

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Loader from '../common/Loader';
+import { ErrorToast, SuccessToast } from '../../utils/Toast';
 
 const Login = () => {
     const [isLoading, setIsLoading] = useState(false);
